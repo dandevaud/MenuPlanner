cb6fc2539196f344095efdb074492f166202eb0c
Dan <dan@ddev.ch>
2020-12-09 19:29:25 +0100

    Dependency Injection set up
    - Added SqlUnity Binder
    - Moved Class to Implementation Folder
    
    Related Work Items: #5

--- SQLImplementation/Implementation/SQLiteSQLConnection.cs
+++ SQLImplementation/Implementation/SQLiteSQLConnection.cs
@@ -8,7 +8,7 @@ using System.Data.SQLite;
 using System.IO;
 using SQLImplementation.contracts;
 
-namespace SQLImplementation
+namespace SQLImplementation.Implementation
 {
     public class SqLiteSqlConnection : ISqlConnection
     {
---
+++ SQLImplementation/IoC/SqlUnityBinder.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="UnityBinder.cs" company="Alessandro Marra & Daniel Devaud">
+// Copyright (c) Alessandro Marra & Daniel Devaud.
+// </copyright>
+
+using SQLImplementation.contracts;
+using SQLImplementation.Implementation;
+using Unity;
+
+namespace SQLImplementation.IoC
+{
+    public class SqlUnityBinder
+    {
+        public static IUnityContainer bind(IUnityContainer unityContainer)
+        {
+            unityContainer.RegisterType<ISqlConnection, SqLiteSqlConnection>();
+
+            return unityContainer;
+        }
+    }
+}
