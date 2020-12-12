6fd34b738c56beadada1d8d8b1693f7a089876f8
Dan <dan@ddev.ch>
2020-12-12 12:09:24 +0100

    SQL Implementation using Entity Framework + Added IoC Container (using BlazorDI)
    - Added MenuPlannerContext
    - set ConnectionStrings in appsettings.json
    - Configured startup to add DBContextFactory & bind IoC

---
+++ MenuPlanner/Server/SqlImplementation/MenuPlannerContext.cs
@@ -0,0 +1,14 @@
+﻿using MenuPlanner.Shared.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MenuPlanner.Server.SqlImplementation
+{
+    public class MenuPlannerContext : DbContext
+    {
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
