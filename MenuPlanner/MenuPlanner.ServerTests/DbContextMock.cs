cf2044d543c82d97e44216db73a3f52514836677
Dan <dan@ddev.ch>
2020-12-26 15:30:25 +0100

    Unit Mock DB Setup
    - Adapted DbContextMock for Async Calls
    - Added DBAsyncQueryProviders

--- MenuPlanner/MenuPlanner.ServerTests/DbContextMock.cs
+++ MenuPlanner/MenuPlanner.ServerTests/DbContextMock.cs
@@ -3,25 +3,37 @@
 // </copyright>
 
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
+using MenuPlanner.Shared.models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 using Moq;
 
 namespace MenuPlanner.ServerTests
 {
     //https://medium.com/@briangoncalves/dbcontext-dbset-mock-for-unit-test-in-c-with-moq-db5c270e68f3
+    //https://docs.microsoft.com/en-us/ef/ef6/fundamentals/testing/mocking
     public static class DbContextMock
     {
-        public static DbSet<T> GetQueryableMockDbSet<T>(List<T> sourceList) where T : class
+        public static Mock<DbSet<T>> GetQueryableMockDbSet<T>(List<T> sourceList) where T : class
         {
             var queryable = sourceList.AsQueryable();
             var dbSet = new Mock<DbSet<T>>();
-            dbSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
+            dbSet.As<IDbAsyncEnumerable<T>>()
+                .Setup(m => m.GetAsyncEnumerator())
+                .Returns(new TestDbAsyncEnumerator<T>(queryable.GetEnumerator()));
+
+            dbSet.As<IQueryable<T>>()
+                .Setup(m => m.Provider)
+                .Returns(new TestDbAsyncQueryProvider<T>(queryable.Provider));
+
             dbSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
             dbSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
-            dbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(() => queryable.GetEnumerator());
-            dbSet.Setup(d => d.Add(It.IsAny<T>())).Callback<T>((s) => sourceList.Add(s));
-            return dbSet.Object;
+            dbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(queryable.GetEnumerator());
+
+            //dbSet.Setup(d => d.Add(It.IsAny<T>())).Callback<T>((s) => sourceList.Add(s));
+            return dbSet;
         }
     }
 }
