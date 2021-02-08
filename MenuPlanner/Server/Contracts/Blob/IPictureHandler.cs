26bdcb58066822916345be51fa0273b11be5a6cb
Dan <dan@ddev.ch>
2021-02-08 09:43:16 +0100

    Added Picture Handler

---
+++ MenuPlanner/Server/Contracts/Blob/IPictureHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using MenuPlanner.Shared.models;
+using Microsoft.AspNetCore.Http;
+
+namespace MenuPlanner.Server.Contracts.Blob
+{
+    public interface IPictureHandler
+    {
+        public Task<string> SaveImage(Guid menuId, Image image);
+
+        public bool ImageExists(Guid menuId, Image image);
+
+        /// <summary>Deletes the image.</summary>
+        /// <param name="menuId">The menu identifier.</param>
+        /// <param name="imageId">The image identifier, if null or empty all images of the menu are deleted</param>
+        public bool DeleteImage(Guid menuId, Guid imageId);
+    }
+}
---
+++ MenuPlanner/Server/Logic/Blob/PictureHandler.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using MenuPlanner.Server.Contracts.Blob;
+using MenuPlanner.Shared.models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.FileProviders;
+
+namespace MenuPlanner.Server.Logic.Blob
+{
+    public class PictureHandler : IPictureHandler
+    {
+
+        private IWebHostEnvironment _hostingEnvironment;
+      
+        public PictureHandler(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        private string GetPath()
+        {   
+            return _hostingEnvironment.WebRootPath;
+        }
+
+        private string GetRelativePath(Guid menuId, Guid imageId)
+        {
+            if (imageId.Equals(Guid.Empty))
+            {
+                return Path.Combine("imageUploads", menuId.ToString());
+            }
+            else
+            {
+                return Path.Combine("imageUploads", menuId.ToString(), imageId.ToString());
+            }
+            
+        }
+
+        public async Task<string> SaveImage(Guid menuId, Image image)
+        {
+            var relativePath = GetRelativePath(menuId, image.Id);
+            var path = Path.Combine(GetPath(), relativePath);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            await using (var fileStream = System.IO.File.Create(Path.Combine(path, image.AlternativeName)))
+            {
+                await fileStream.WriteAsync(image.ImageBytes);
+               
+            }
+            return Path.Combine(relativePath, image.AlternativeName);
+        }
+
+        public bool DeleteImage(Guid menuId, Guid imageId)
+        {
+           
+           
+              var path = Path.Combine(GetPath(), GetRelativePath(menuId, imageId));
+            
+
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path,true);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool ImageExists(Guid menuId, Image image)
+        {
+            
+            return Directory.Exists(Path.Combine(GetPath(), GetRelativePath(menuId, image.Id)));
+        }
+    }
+}
