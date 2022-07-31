27e9d1a0f0a2f3bc1fbb8a022498757888d21f57
Daniel Devaud <devaudan@students.zhaw.ch>
2022-07-31 15:15:17 +0200

    Added Auth with JWT

--- MenuPlanner/Client/Program.cs
+++ MenuPlanner/Client/Program.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using MenuPlanner.Client.Shared;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MenuPlanner.Client
@@ -17,16 +18,25 @@ namespace MenuPlanner.Client
 
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddHttpClient("MenuPlanner.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+            
             //https://chrissainty.com/avoiding-accesstokennotavailableexception-when-using-blazor-webassembly-hosted-template-with-individual-user-accounts/
             builder.Services.AddHttpClient<PublicClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
+            
             // Supply HttpClient instances that include access tokens when making requests to the server project
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("MenuPlanner.ServerAPI"));
+            builder.Services.AddScoped<CustomAuthorizationMessageHandler>();
+
+            builder.Services.AddHttpClient("MenuPlanner.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<CustomAuthorizationMessageHandler>();
+
             builder.Services.AddApiAuthorization(options =>
             {
                 options.ProviderOptions.ConfigurationEndpoint = "oidc.json";
+            });
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                builder.Configuration.Bind("oidc", options.ProviderOptions);
             });
 
             builder.Services.AddCors(options =>
@@ -35,6 +45,7 @@ namespace MenuPlanner.Client
                     builder =>
                     {
                         builder.WithOrigins("http://sts.ddev.ch");
+                        builder.WithOrigins("http://sts-uat.ddev.ch");
                     });
             });
 
