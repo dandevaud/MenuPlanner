539d8d21b754abef377a65ee9533e0771ce6528e
Daniel Devaud <devaudan@students.zhaw.ch>
2022-07-31 15:15:44 +0200

    Replaced Removed Extension Methods + Refactoring

--- MenuPlanner/Server/Data/ApplicationDbContext.cs
+++ MenuPlanner/Server/Data/ApplicationDbContext.cs
@@ -2,7 +2,7 @@
 // Copyright (c) Alessandro Marra & Daniel Devaud.
 // </copyright>
 
-using IdentityServer4.EntityFramework.Options;
+using Duende.IdentityServer.EntityFramework.Options;
 using MenuPlanner.Server.Models;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
